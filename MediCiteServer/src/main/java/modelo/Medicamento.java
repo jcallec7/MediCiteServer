8edcd37899982bd077f68b658a090ff74cbc7994
tatcjho <tatycjho@gmail.com>
2020-01-16 17:34:56 -0500

    Medicamento con codigo AUTO

--- MediCiteServer/src/main/java/modelo/Medicamento.java
+++ MediCiteServer/src/main/java/modelo/Medicamento.java
@@ -7,6 +7,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -19,6 +21,7 @@ public class Medicamento implements Serializable{
 	
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="mc_medic_id")
 	private int id;
 	
