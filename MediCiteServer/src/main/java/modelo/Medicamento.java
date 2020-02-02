eacf6748915fa045dbe8fb110fcd502a9aa65fe4
tatcjho <tatycjho@gmail.com>
2020-02-02 13:21:52 -0500

    Validacion prueba lista en medicamento, falta icono de advertencia

--- MediCiteServer/src/main/java/modelo/Medicamento.java
+++ MediCiteServer/src/main/java/modelo/Medicamento.java
@@ -3,6 +3,9 @@ package modelo;
 import java.util.List;
 import java.io.Serializable;
 
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,8 +15,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 
 
+@ManagedBean
+@RequestScoped
 @Entity
 public class Medicamento implements Serializable{
 	
@@ -24,6 +30,7 @@ public class Medicamento implements Serializable{
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="mc_medic_id")
 	private int id;
+	
 	
 	@Column(name="mc_medic_nombre")
 	private String nombre;
@@ -38,12 +45,15 @@ public class Medicamento implements Serializable{
 	public void setId(int id) {
 		this.id = id;
 	}
+		
 	public String getnombre() {
 		return nombre;
 	}
 	public void setnombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	
 	public String getConcentracion() {
 		return concentracion;
 	}
