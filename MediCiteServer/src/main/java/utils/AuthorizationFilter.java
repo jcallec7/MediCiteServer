d57c3e23032fefc0251f2ec3cbe6c66b7f211a45
jcall <jcall@DELL-JOSE.mshome.net>
2020-02-08 13:31:20 -0500

    .

--- MediCiteServer/src/main/java/utils/AuthorizationFilter.java
+++ MediCiteServer/src/main/java/utils/AuthorizationFilter.java
@@ -35,7 +35,7 @@ public class AuthorizationFilter implements Filter {
 			String reqURI = reqt.getRequestURI();
 			if (reqURI.indexOf("/login.xhtml") >= 0
 					||(ses != null && ses.getAttribute("user") != null)
-					|| reqURI.indexOf("/public/") >= 0
+					|| reqURI.indexOf("/publicas/") >= 0
 					|| reqURI.contains("javax.faces.resource"))
 				chain.doFilter(request, response);
 			else
